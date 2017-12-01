a1a503582cdcd9e6e34428a62636a1090bc394b0
Joseph Rawson <joseph.rawson.works@gmail.com>
2017-12-01 12:26:05 -0600

    update initdb script

--- gillie/scripts/initializedb.py
+++ gillie/scripts/initializedb.py
@@ -15,18 +15,15 @@ from ..models import (
     get_session_factory,
     get_tm_session,
     )
-from ..models import MyModel
-from ..models.uzig import User, Group, UserGroup
-from ..models.uzig import GroupPermission
+#from ..models import MyModel
 
-
+from ..models import usergroup
 
 def usage(argv):
     cmd = os.path.basename(argv[0])
     print(('usage: %s <config_uri> [var=value]\n'
           '(example: "%s development.ini")' % (cmd, cmd)))
     sys.exit(1)
-
 
 def main(argv=sys.argv):
     if len(argv) < 2:
@@ -37,42 +34,45 @@ def main(argv=sys.argv):
     settings = get_appsettings(config_uri, options=options)
 
     engine = get_engine(settings)
-    #Base.metadata.create_all(engine)
+    #import pdb ; pdb.set_trace()
+    
+    Base.metadata.create_all(engine)
 
     session_factory = get_session_factory(engine)
 
     with transaction.manager:
         dbsession = get_tm_session(session_factory, transaction.manager)
 
-        model = MyModel(name='one', value=1)
-        dbsession.add(model)
+        #model = MyModel(name='one', value=1)
+        #dbsession.add(model)
 
+        usergroup.populate(dbsession)
+        user_stuff = """
+        #admins = Group(group_name="admins")
+        #group_permission = GroupPermission(perm_name='root_administration')
+        #dbsession.add(admins)
+        #admins.permissions.append(group_permission)
+        #dbsession.flush()
 
-        admins = Group(group_name="admins")
-        group_permission = GroupPermission(perm_name='root_administration')
-        dbsession.add(admins)
-        admins.permissions.append(group_permission)
-        dbsession.flush()
-
-        users = Group(group_name="users")
-        dbsession.add(users)
-        dbsession.flush()
+        #users = Group(group_name="users")
+        #dbsession.add(users)
+        #dbsession.flush()
 
     
         
         #admin = User(user_name='admin', name='Admin User',
         #            email='admin@localhost')
-        admin = User()
-        admin.user_name = 'admin'
-        admin.email = 'admin@localhost'
-        admin.name = 'Admin User'
+        #admin = User()
+        #admin.user_name = 'admin'
+        #admin.email = 'admin@localhost'
+        #admin.name = 'Admin User'
         
-        dbsession.add(admin)
-        dbsession.flush()
-        admin.set_password('admin')
-        admin.regenerate_security_code()
-        admins.users.append(admin)
-        dbsession.flush()
+        #dbsession.add(admin)
+        #dbsession.flush()
+        #admin.set_password('admin')
+        #admin.regenerate_security_code()
+        #admins.users.append(admin)
+        #dbsession.flush()
 
         ADMIN_PERMISSIONS = ['read', 'edit', 'create', 'delete']
         for perm in ADMIN_PERMISSIONS:
@@ -97,4 +97,6 @@ def main(argv=sys.argv):
             ug = UserGroup(group_id=users.id, user_id=u.id)
             dbsession.add(ug)
             dbsession.flush()
+        """
+        
 
