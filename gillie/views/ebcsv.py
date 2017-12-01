162c69251b4c2bdf8e2637c0324f60ef11ed9765
Joseph Rawson <joseph.rawson.works@gmail.com>
2017-12-01 12:24:46 -0600

    added simple model view

---
+++ gillie/views/ebcsv.py
@@ -0,0 +1,54 @@
+import os
+from configparser import ConfigParser
+from datetime import datetime
+from urllib.error import HTTPError
+
+from cornice.resource import resource, view
+from pyramid.httpexceptions import HTTPNotFound
+from pyramid.httpexceptions import HTTPFound
+from pyramid.httpexceptions import HTTPForbidden
+from bs4 import BeautifulSoup
+from sqlalchemy.orm.exc import NoResultFound
+import transaction
+
+from .restviews import BaseResource
+from .restviews import SimpleModelResource
+
+from ..models.util import TimeStampMixin
+
+
+APIROOT = '/api/dev/bapi'
+
+rscroot = os.path.join(APIROOT, 'main')
+
+from pyramid.view import view_config
+from pyramid.response import Response
+import requests
+
+from alchemyjsonschema import SchemaFactory
+from alchemyjsonschema import NoForeignKeyWalker
+
+from ..models.mymodel import EBMODELS
+
+def get_clzpage(request):
+    path = os.path.join(*request.matchdict['path'])
+    url = os.path.join('http://core.collecterz.com', path)
+    r = requests.get(url)
+    return Response(r.content)
+    
+    
+
+modelpath = os.path.join(APIROOT, 'sofi', '{model}')
+@resource(collection_path=modelpath,
+          path=os.path.join(modelpath, '{id}'))
+class EbClzView(SimpleModelResource):
+    def __init__(self, request, context=None):
+        super(EbClzView, self).__init__(request, context=context)
+        self.limit = 25
+        self._use_pagination = True
+        self.model = EBMODELS.get(self.request.matchdict['model'])
+        self.factory = SchemaFactory(NoForeignKeyWalker)
+
+    @property
+    def model_map(self):
+        return EBMODELS
--- gillie/views/restviews.py
+++ gillie/views/restviews.py
@@ -1,21 +1,22 @@
 import os
 from datetime import datetime
 
+from sqlalchemy import func
 import transaction
 from cornice.resource import resource, view
 
+from ..models.util import TimeStampMixin
 
 def apiroot(prefix='/api', version='dev'):
     return os.path.join(prefix, version)
 
-
 class BaseResource(object):
     def __init__(self, request, context=None):
-        super(BaseResource, self).__init__(request, context=context)
         self.request = request
         self.db = self.request.dbsession
-        self.limit = None
+        self.limit = 25
         self.max_limit = 100
+        self._use_pagination = True
         
     def serialize_object(self, dbobj):
         return dbobj.serialize()
@@ -49,4 +50,69 @@ class BaseResource(object):
         data = [self.serialize_object_for_collection_query(o) for o in objects]
         return dict(total_count=total_count, items=data)
     
+# /api/{model}/{id}
+class SimpleModelResource(BaseResource):
+    def __init__(self, request, context=None):
+        super(SimpleModelResource, self).__init__(request, context=context)
+        self.model = self.model_map.get(self.request.matchdict['model'])
 
+    @property
+    def model_map(self):
+        raise NotImplementedError
+
+    def _isTimeStampMixin(self):
+        return isinstance(self.model(), TimeStampMixin)
+
+    def collection_query(self):
+        return self.db.query(self.model)
+    
+    def collection_post(self):
+        with transaction.manager:
+            m = self.model()
+            for field in self.request.json:
+                value = self.request.json[field]
+                if type(value) is dict:
+                    print("value of field {} is dict".format(field))
+                setattr(m, field, value)
+            self.db.add(m)
+            self.db.flush()
+        return self.serialize_object(m)
+
+    def get(self):
+        id = self.request.matchdict['id']
+        m = self.db.query(self.model).get(id)
+        if m is None:
+            raise HTTPNotFound
+        return self.serialize_object(m)
+
+    
+    def put(self):
+        with transaction.manager:
+            id = self.request.matchdict['id']
+            m = self.db.query(self.model).get(id)
+            if m is None:
+                raise HTTPNotFound
+            fields = set(self.request.json.keys())
+            if self._isTimeStampMixin():
+                fields.discard('created_at')
+                fields.discard('updated_at')
+                m.updated_at = func.now()
+            import pdb ; pdb.set_trace()
+            for field in fields:
+                setattr(m, field, self.request.json[field])
+            self.db.add(m)
+            self.db.flush()
+        return self.serialize_object(m)
+    
+    def delete(self):
+        with transaction.manager:
+            id = self.request.matchdict['id']
+            m = self.db.query(self.model).get(id)
+            if m is None:
+                raise HTTPNotFound
+            m.delete()
+        
+          
+          
+    
+        
