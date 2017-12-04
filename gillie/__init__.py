f8047b6f3f59196bf0731f9168b78bb2cd5444e5
Joseph Rawson <joseph.rawson.works@gmail.com>
2017-12-04 00:43:35 -0600

    move more view and route config to routes module

--- gillie/__init__.py
+++ gillie/__init__.py
@@ -31,7 +31,6 @@ def main(global_config, **settings):
     config.include('pyramid_mako')
     config.include('cornice')
     config.include('.models')
-    config.include('.routes')
 
     renderer = JSON()
     renderer.add_adapter(datetime.date, lambda obj, request: obj.isoformat())
@@ -52,31 +51,22 @@ def main(global_config, **settings):
     authz_policy = ACLAuthorizationPolicy()
     config.set_authorization_policy(authz_policy)
 
+    config.include('.routes')
 
-    config.add_route('home', '/')
-    config.add_route('apps', '/app/{appname}')
-    config.add_route('admin', '/admin')
+    config.add_route('meeting_calendar', '/rest/v0/main/hubcal')
+    config.add_view('hubby.views.main.MeetingCalendarViewer',
+                    route_name='meeting_calendar',
+                    renderer='json',)
     
-    config.add_route('login', '/login')
-    config.add_route('auth_refresh', '/auth/refresh')
-
-    config.add_view('.views.userauth.login', route_name='login',
-                    request_method='POST', renderer='json')
-    config.add_view('.views.userauth.refresh', route_name='auth_refresh',
-                    request_method='GET', renderer='json')
-
-    #scan_views = ['notfound', 'sitecontent', 'userauth', 'todos']
-    # FIXME jsonapi has "notfound" view
-    scan_views = ['sitecontent', 'userauth', 'todos',
-                  'client',
-                  'ebcsv',
-                  'wikipages',]
-    if not use_pj:
-        scan_views.append('notfound')
-    for view in scan_views:
-        config.scan('.views.%s' % view)
-
+    config.add_route('meeting_calendar_ts', '/hubcal1')
+    config.add_view('hubby.views.main.MeetingCalendarViewer',
+                    route_name='meeting_calendar_ts',
+                    renderer='json',)
     
+    hubby_views = ['basic', 'main']
+    for view in hubby_views:
+        config.scan('hubby.views.{}'.format(view))
+
     #config.add_route('clzcore', '/clzcore/*path')
     #config.add_view('.views.wikipages.get_clzpage', route_name='clzcore')
     
--- gillie/routes.py
+++ gillie/routes.py
@@ -1,4 +1,37 @@
+from paste.deploy.converters import asbool
+
 def includeme(config):
+    settings = config.get_settings()
+
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_static_view('assets', '../assets', cache_max_age=3600)
-    #config.add_route('home', '/')
+
+    config.add_route('home', '/')
+    config.add_route('apps', '/app/{appname}')
+    config.add_route('admin', '/admin')
+    
+    config.add_route('login', '/login')
+    config.add_route('auth_refresh', '/auth/refresh')
+    config.add_route('auth_chpass', '/auth/chpass')
+
+
+    config.add_view('.views.userauth.login', route_name='login',
+                    request_method='POST', renderer='json')
+    config.add_view('.views.userauth.refresh', route_name='auth_refresh',
+                    request_method='GET', renderer='json')
+    config.add_view('.views.userauth.chpass', route_name='auth_chpass',
+                    request_method='POST', renderer='json')
+
+    use_pj = asbool(settings.get('api.use_pyramid_jsonapi', False))
+    # FIXME jsonapi has "notfound" view
+    #scan_views = ['notfound', 'sitecontent', 'userauth', 'todos']
+    scan_views = ['sitecontent', 'userauth', 'todos',
+                  'client',
+                  'wikipages',
+                  'ebcsv',]
+    if not use_pj:
+        scan_views.append('notfound')
+    for view in scan_views:
+        config.scan('.views.%s' % view)
+
+    
