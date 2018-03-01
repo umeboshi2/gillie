a727ecac304bf570c0d9549d7fb9b08ce353b887
Joseph Rawson <joseph.rawson.works@gmail.com>
2018-03-01 13:32:32 -0600

    cleanup gillie init

--- gillie/__init__.py
+++ gillie/__init__.py
@@ -20,7 +20,7 @@ def groupfinder(userid, request):
         groups = ['group:%s' % g.id for g in request.user.groups]
         return groups
     return []
-    
+
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -39,7 +39,7 @@ def main(global_config, **settings):
     JWT_SECRET = os.environ.get('JWT_SECRET', 'secret')
     config.set_jwt_authentication_policy(JWT_SECRET,
                                          callback=groupfinder)
-    
+
     authz_policy = ACLAuthorizationPolicy()
     config.set_authorization_policy(authz_policy)
 
@@ -47,33 +47,13 @@ def main(global_config, **settings):
     if True or use_pj:
         from .models.usergroup import User, Group
         pj = pyramid_jsonapi.PyramidJSONAPI(config, [User, Group])
-        ep = pj.endpoint_data.endpoints
-    
+        # ep = pj.endpoint_data.endpoints
         pj.create_jsonapi()
-    
+
     config.include('.routes')
 
-    config.add_route('meeting_calendar', '/rest/v0/main/hubcal')
-    config.add_view('hubby.views.main.MeetingCalendarViewer',
-                    route_name='meeting_calendar',
-                    renderer='json',)
-    
-    config.add_route('meeting_calendar_ts', '/hubcal1')
-    config.add_view('hubby.views.main.MeetingCalendarViewer',
-                    route_name='meeting_calendar_ts',
-                    renderer='json',)
-    
-    hubby_views = ['basic', 'main']
-    for view in hubby_views:
-        config.scan('hubby.views.{}'.format(view))
-
-    #config.add_route('clzcore', '/clzcore/*path')
-    #config.add_view('.views.wikipages.get_clzpage', route_name='clzcore')
-    
     config.set_request_property('.util.get_user', 'user', reify=True)
-    
-    
     application = config.make_wsgi_app()
     # add wsgi middleware here
-    
+
     return application
